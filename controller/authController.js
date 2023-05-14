b08f17fd758b6c9d31efeadfd960c9d5fb507322
SoummoSinghoRoy <roysoummo1607@gmail.com>
2023-05-14 21:06:43 +0600

    auth controller, route, validation done, error utils add

--- controller/authController.js
+++ controller/authController.js
@@ -1,13 +1,70 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
+const { resourceError, serverError } = require('../utils/error');
+const db = require('../model/db_config');
+const User = db.user;
 
-exports.signupPostController = (req, res) => {
-  const { username, email, password, confirmPassword } = req.body;
+exports.signupPostController = async (req, res) => {
+  const { username, email, password } = req.body;
   const errors = validationResult(req).formatWith(err => err.msg)
 
-  if(!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
     return res.status(400).json({
       errors: errors.mapped()
     })
+  }
+
+  try {
+    const registered_user = await User.create({
+      username, email, password
+    })
+    res.status(200).json({
+      Message: "User created successfull",
+      registered_user
+    })
+  } catch (error) {
+    serverError(res, error)
+  }
+}
+
+exports.loginPostController = async (req, res) => {
+  const { email, password } = req.body;
+  const errors = validationResult(req).formatWith(err => err.msg)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.mapped()
+    })
+  }
+
+  try {
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      resourceError(res, "User not found")
+    } else {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err) {
+          return serverError(res, err)
+        }
+        if (!result) {
+          return resourceError(res, "Incorrect password")
+        }
+        const token = jwt.sign({
+          username: user.username,
+          email: user.email
+        }, config.get('secret'), { expiresIn: "10h" })
+
+        res.status(200).json({
+          Message: "Successfully logged in",
+          token: `Bearer ${token}`
+        })
+      })
+    }
+  } catch (error) {
+    serverError(res, error)
   }
 }
--- router/authRoute.js
+++ router/authRoute.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 
-const { signupPostController } = require('../controller/authController');
+const { signupPostController, loginPostController } = require('../controller/authController');
 const signupValidation = require('../validator/auth/signupValidation');
+const loginValidation = require('../validator/auth/loginValidation');
 
 router.post('/signup', signupValidation, signupPostController);
-router.post('/login');
+router.post('/login', loginValidation, loginPostController);
 
 module.exports = router;
